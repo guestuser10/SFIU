1b816af004770b18271f21c08441ccddbfbb901e
guestuser10 <usuarioguest10@gmail.com>
2023-11-01 14:15:28 -0700

    nav.preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StandaloneNavComponent } from './standalone-nav/standalone-nav.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StandaloneNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
+    MatToolbarModule, 
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

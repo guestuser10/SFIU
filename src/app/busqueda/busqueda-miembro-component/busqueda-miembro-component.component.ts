595284bcd173527edb05feae07ad82e2d07e8c12
guestuser10 <usuarioguest10@gmail.com>
2023-11-22 23:55:41 -0700

    Add search filters and styling to member search component

--- src/app/busqueda/busqueda-miembro-component/busqueda-miembro-component.component.ts
+++ src/app/busqueda/busqueda-miembro-component/busqueda-miembro-component.component.ts
@@ -1,10 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AngMatImpModule } from 'src/app/ang-mat-imp/ang-mat-imp.module';
-// Define una interfaz para representar la estructura del JSON esperado del servidor
+
 interface MiembroResponse {
-  Creyentes?: any[]; // Puedes ajustar esto según la estructura real del JSON
+  Creyentes?: any[];
 }
 
 @Component({
@@ -13,30 +12,29 @@ interface MiembroResponse {
   styleUrls: ['./busqueda-miembro-component.component.css']
 })
 export class BusquedaMiembroComponentComponent implements OnInit {
-  miembros: any[] = []; // Inicializa la variable con un array vacío
+  miembros: any[] = [];
+  busqueda: string = '';
+  orden: 'asc' | 'desc' = 'asc';
 
-  constructor(private http: HttpClient, private AR: ActivatedRoute, private router:Router) { }
-  
+  constructor(private http: HttpClient, private AR: ActivatedRoute, private router: Router) {}
+
   ngOnInit(): void {
-    var busqueda: string ;
+    // Extrae el valor de 'busqueda' de la URL
+    this.busqueda = this.AR.snapshot.params['busqueda'];
 
-    busqueda = this.AR.snapshot.params['busqueda'];
-
-    this.buscarMiembros(busqueda)
+    // Inicia la búsqueda al cargar el componente
+    this.buscarMiembros();
   }
-  buscarMiembros(busqueda: string) {
-    // Utiliza la URL proporcionada para realizar la búsqueda
-    const url = `http://127.0.0.1:8000/miembros/${busqueda}`;
-    console.log(url);
+
+  buscarMiembros() {
+    const url = `http://127.0.0.1:8000/miembros/${this.busqueda}`;
     this.http.get<MiembroResponse>(url).subscribe(
       response => {
-        // Verifica si la respuesta contiene la clave "Creyentes"
         if (response.Creyentes) {
-          // Transforma el formato del array si es necesario
           this.miembros = response.Creyentes.map((creyente: any) => {
             return {
               id: creyente.id,
-              nombre: creyente.nombre || '',  // Asigna un valor predeterminado si es undefined
+              nombre: creyente.nombre || '',
               telefono: creyente.telefono,
               direccion: creyente.direccion,
               dias_disp: creyente.dias_disp,
@@ -44,25 +42,40 @@ export class BusquedaMiembroComponentComponent implements OnInit {
               activo: creyente.activo
             };
           });
+
+          // Aplica el orden
+          this.miembros.sort((a, b) => {
+            const factor = (this.orden === 'asc') ? 1 : -1;
+            return factor * a.nombre.localeCompare(b.nombre);
+          });
+
         } else if (Array.isArray(response)) {
-          // Usa el formato existente si la respuesta es un array
           this.miembros = response;
+
+          // Aplica el orden
+          this.miembros.sort((a, b) => {
+            const factor = (this.orden === 'asc') ? 1 : -1;
+            return factor * a.nombre.localeCompare(b.nombre);
+          });
+
         } else {
-          // En otros casos, maneja la respuesta según tus necesidades
           console.error('Formato de respuesta no reconocido:', response);
         }
       },
       error => {
         console.error('Error al obtener datos de miembros:', error);
+        // Agrega lógica adicional para manejar errores, por ejemplo, mostrar un mensaje al usuario.
       }
     );
   }
 
-  redirigirAPerfil(idCreyente: number) {
-    // Construir la ruta con el id_creyente
-    const rutaPerfil = `/perfil/${idCreyente}`;
+  cambiarOrden(nuevoOrden: 'asc' | 'desc') {
+    this.orden = nuevoOrden;
+    this.ngOnInit();
+  }
 
-    // Redirigir a la ruta perfil
+  redirigirAPerfil(idCreyente: number) {
+    const rutaPerfil = `/perfil/${idCreyente}`;
     this.router.navigate([rutaPerfil]);
   }
 }

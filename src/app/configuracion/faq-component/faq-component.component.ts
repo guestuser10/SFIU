597337c36ab0b9063da4fb56a1f03bd90ef937a2
guestuser10 <usuarioguest10@gmail.com>
2023-12-05 20:15:53 -0700

    Refactor FAQ component and fetch FAQs from API

--- src/app/configuracion/faq-component/faq-component.component.ts
+++ src/app/configuracion/faq-component/faq-component.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-faq-component',
   templateUrl: './faq-component.component.html',
   styleUrls: ['./faq-component.component.css']
 })
-export class FAQComponentComponent {
-  faqs = [
-    { question: 'Pregunta 1', answer: 'Respuesta 1', active: false },
-    { question: 'Pregunta 2', answer: 'Respuesta 2', active: false },
-    { question: 'Pregunta 3', answer: 'Respuesta 3', active: false },
-    { question: '¿no se encuentra la respuesta?', answer: 'contactanos: correo@gmail.com', active: false }
-  ];
+export class FAQComponentComponent implements OnInit {
+  faqs: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchFAQs();
+  }
+
+  fetchFAQs() {
+    this.http.get<any>('http://127.0.0.1:8000/faq').subscribe(data => {
+      this.faqs = data.faq;
+    });
+  }
 
   toggleFaq(faq: any) {
     faq.active = !faq.active;
